38ef6e69341152a93e2b1e84a5fe269ebe5fff0d
mohamed <mohamedboukab202@gmail.com>
2025-10-08 16:25:48 +0100

    feat: implement create, get all, get by ID and delete user endpoints

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const express = require('express');
 const connectDB = require('./config/database');
 const PORT = process.env.PORT;
 
+const usersRouter = require("./routes/users");
+
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/users", usersRouter);
+
 
 
 app.get('/', (req, res) => {
