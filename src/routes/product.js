69cc2aecf6f52d04cb6582bcabbd53428262c750
mohamed <mohamedboukab202@gmail.com>
2025-10-10 09:29:00 +0100

    feat: add request validation using Yup middleware

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const validator = require("../middlewares/validationMiddleware");
+const { productSchema } = require("../middlewares/schema");
 
-
-router.post("/", productController.createProduct);
+router.post("/", validator.validate(productSchema), productController.createProduct);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
-router.put("/:id", productController.updateProduct);
+router.put("/:id", validator.validate(productSchema), productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const validator = require("../middlewares/validationMiddleware");
+const { userSchema } = require("../middlewares/schema");
 
-
-router.post("/", userController.createUser);
+router.post("/", validator.validate(userSchema), userController.createUser);
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.delete("/:id", userController.deleteUser);
